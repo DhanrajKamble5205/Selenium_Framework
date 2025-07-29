4e0000a880dd3854a57b954094856fa2160ff1cc
Dhanraj <Dhanraj@DESKTOP-RM8I8R7>
2025-07-29 20:41:45 +0530

    added

--- Selenium_Framework/src/main/java/base/BaseTC.java
+++ Selenium_Framework/src/main/java/base/BaseTC.java
@@ -1,5 +1,38 @@
 package base;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
 public class BaseTC {
 
+	public static Properties config;
+	public static WebDriver driver;
+	
+	public void driverintiliztion() {
+		driver = new ChromeDriver();
+		driver.get(config.getProperty("Url"));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(Integer.parseInt(config.getProperty("pageloadTime"))));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+	}
+
+	public void readPropertyFile() throws IOException {
+		try {
+			FileInputStream fileinput = new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\java\\resources\\Property\\config.properties");
+			config = new Properties();
+			config.load(fileinput);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	}
 }
--- Selenium_Framework/src/main/java/pageEvent/HomePage.java
+++ Selenium_Framework/src/main/java/pageEvent/HomePage.java
@@ -1,5 +1,20 @@
 package pageEvent;
 
-public class HomePage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+import base.BaseTC;
+
+public class HomePage extends BaseTC{
+	
+	@FindBy(xpath = "//a[@href='/flights/']/span[2]")
+	WebElement flight;
+
+	@FindBy(xpath ="//li[contains(text(),'Round Trip')]")
+	WebElement RoundTrip;
+	
+	@FindBy(xpath ="//li[contains(text(),'Multi City')]")
+	WebElement MultiCity;
+	
+	
 }
